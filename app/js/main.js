ce89d0bc824b8e52377749516e9977dab687eff4
Danil1906 <Danilkozlov36@gmail.com>
2022-05-29 02:13:51 +0300

    add mobile adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,14 @@ $(function () {
         autoplaySpeed: 2000,
         prevArrow: '<button class="general-arrows slider-arrow-prev"></button>',
         nextArrow: '<button class="general-arrows slider-arrow-next"></button>',
+        responsive: [
+            {
+              breakpoint: 1321,
+              settings: {
+                arrows: false,
+              }
+            },
+          ]
     });
 
     // product cart animation
@@ -30,12 +38,12 @@ $(function () {
 
     });
 
-    $('.about__title-sert').on('click', function (e) {
+    $('.about__title-serttext').on('click', function (e) {
 
         let sert_block = $('.about__images-serf')
         $(sert_block).slideToggle()
 
-        $(this).toggleClass('about__title-sert--active')
+        $(this).toggleClass('about__title-serttext--active')
 
     });
 
@@ -46,26 +54,41 @@ $(function () {
         }
     });
 
-    
+
     let all_products_btn = document.querySelectorAll('.products__item-btn')
-    all_products_btn.forEach(function(elems){
+    all_products_btn.forEach(function (elems) {
         // Тут нузно взять титле и поместить
         let input_text = ''
 
         // text_for_form
-        $(elems).on('click', function(){
+        $(elems).on('click', function () {
             let name_prod = $(elems).closest('.products__item-btnbox').siblings('.products__item-textinfo').find('.products__item-name').text()
             let form_text = $('.text_for_form').text()
             input_text = form_text.replace('$', name_prod)
 
-            
+
             $('.popup-form__textarea').val(input_text)
         });
-        
+
         $(elems).magnificPopup({
-            type:'inline',
+            type: 'inline',
             midClick: true
         });
 
     })
+
+    $('a[href^="#"]').click(function (e) {
+        e.preventDefault()
+        let anchor = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top
+        }, 600);
+    });
+
+    // burger btn 
+        $('.header__burger-btn').click(function(){
+            $(this).toggleClass('open');
+            $('.header-mobile').toggleClass('header-mobile--open')
+        });
+
 })
